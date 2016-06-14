1233674b9e8b609cb47a01a8efc64ee1600bb086
Bjoern Griebenow <b.griebenow@web.de>
2016-06-14 15:18:53 +0200

    Script can replace multible regular expressions

---
+++ test_input.cpp
@@ -0,0 +1,23 @@
+
+#include <windows.h>
+
+using namespace std;
+
+FLOAT a;
+vector<UINT> b;
+vector<SHORT> b;
+map<string> c;
+map<std::queue<wstring>> c;
+
+int main(void)
+{
+    int c1;
+    float c2;
+
+    SHORT d;
+    USHORT e;
+    SHORT f;
+    USHORT g;
+
+    return 0;
+}
